b68ae6b23776f72cca68adf9485c1f10ba457797
HCPP <76736848+HCPP20334@users.noreply.github.com>
2024-07-01 17:44:02 +0300

    Add files via upload
    Signed-off-by: HCPP  <76736848+HCPP20334@users.noreply.github.com>

--- WEGL.js
+++ WEGL.js
@@ -1,3 +1,5 @@
+
+
 function setup()
 {
     let canvas = createCanvas(300,600,WEBGL);
@@ -9,8 +11,17 @@ function setup()
     
     
 }
+function SetTx(data)
+{
+    let bufferDebug = document.createElement("p");
+    bufferDebug.style = "position:absolute;z-index:-1;color:white;font-family:'WR';transform:translate(0px,400px)";
+    
+    bufferDebug.innerHTML = data;
+    console.log(data);
+}
 function draw()
 {
+    
     let rd = 100;
     let x,y;
     x++;
@@ -20,7 +31,6 @@ function draw()
     background('rgba(255,255,255, 0)');
     ambientLight(0,255/4,0);
     directionalLight(0,0,255,-1,0,0);
-    
    // spotLight(0, 0, 255, flX, flY,250);
     pointLight(0, 0, 255, flX, flY,250);
 push();
@@ -119,5 +129,14 @@ rotateX(frameCount / rd);
 ambientMaterial(250);
 specularMaterial (250);
 box(50,50,50,50);
-
+const PLAPImem = performance.memory;
+const PLmemoryUsed  = (PLAPImem.usedJSHeapSize / 1024) / 1024;
+const PLmemoryTotal = (PLAPImem.totalJSHeapSize / 1024);
+const PLmemoryJsHeapSize  = (PLAPImem.jsHeapSizeLimit / 1024) / 1024;
+//
+debugData.style.color="white";
+debugData.style ="transform: translateY(100px);"
+debugData.style.fontFamily="WR";
+debugData.innerHTML = "X:"+parseInt(sin(frameCount * 0.04) * 20)+" Y :"+parseInt(sin(frameCount * 0.04) * 20)+" fps:"+parseInt(frameRate())+" mem:"+parseInt(PLmemoryUsed);
+console.log("X:"+sin(frameCount * 0.04) * 20+"Y:"+sin(frameCount * 0.04) * 20+" fps:"+frameCount);
 }
