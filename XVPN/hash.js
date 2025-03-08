6dce1bd7e4c9ac8745aca0b92877e7dc658f6a97
HCPP <76736848+HCPP20334@users.noreply.github.com>
2025-03-08 23:34:50 +0300

    Add files via upload
    Signed-off-by: HCPP  <76736848+HCPP20334@users.noreply.github.com>

---
+++ XVPN/hash.js
@@ -0,0 +1,10 @@
+let strBuffer = "1234567890";
+function genHash(str,len){
+    let out_hash = '';
+    for(let sz_str = 0;sz_str <= len;sz_str++){
+        out_hash += str[Math.floor(Math.random() * str.length)];
+        return out_hash;
+    }
+}
+let hashData = getHash(strBuffer,128);
+console.log(hashData);
--- XVPN/script/main.js
+++ XVPN/script/main.js
@@ -1,30 +1,37 @@
+window.addEventListener('error', function (e) {
+  mainLog("[error]"+e.error);
+ });
+
+let doLoadTimeMs = new Float64Array();
+doLoadTimeMs = parseFloat(new Date().getMilliseconds());
+let toLoadTimeMs = new Float64Array();
 let stringData = [
- "XVPN БАЗОВАЯ",//0
- "XVPN ПРО",//1
- "XVPN МАКС",//2
+ "XVPN БАЗА",//0
+ "XVPN про",//1
+ "XVPN ультра   ",//2
  "@Все права Защищены 2025",//3
  "Гитхаб",//4
  "Донат",//5
- "Работает на Любых устроиствах",//6
- "Ограничение 10 МБИТ",//7
- "Ограничение 100 МБИТ",//8
- "Ограничение 200 МБИТ",//9
- " ПРОТОКОЛ VLESS , ШИФРОВАНИЕ REALITY",//10
- "3$/400 RUB в месяц",//11
- "6$/600 RUB в месяц",//12
- "10$/1000 RUB в месяц",//13
- "XVPN BASE",//14
+ "",//6
+ "10 МБИТ ",//7
+ "1 ГБИТ  ",//8
+ "500 МБИТ",//9
+ "VLESS | REALITY",//10
+ "  200.0 RUB",//11
+ "600.0 RUB",//12
+ "1000.0RUB",//13
+ "XVPN BASE ",//14
  "XVPN BASE+",//15
- "XVPN PRO",//16
+ "XVPN PRO  ",//16
  "@All Right Copyright 2025",//17
- "Speed Limit to 10 MBIT",//18
- "Speed Limit to 100 MBIT",//19
- "Speed Limit to 200 MBIT",//20
- "Work Only Devices",//21
- " PROTOCOL VLESS , CRYPTO REALITY",//22
- "PRICE: 3$/400 RUB per month",//23
- "PRICE: 6$/600 RUB per month",//24
- "PRICE: 10$/1000 RUB per month",//25
+ "10 MBIT",//18
+ "1 GBIT",//19
+ "500 MBIT",//20
+ "",//21
+ "VLESS | REALITY",//22
+ "2.00$",//23
+ "6.00$",//24
+ "10.0$",//25
 ];
 let userDeviceArray = [
   {device: 'Android', platform: /Android/},
@@ -39,6 +46,60 @@ let userDeviceArray = [
 ];
 let  platform = navigator.userAgent;
 let  cssLoader = document.getElementById("cssdata");
+var  main_logStr = '';
+let cntmsg = 0;
+let consoleBuffer = document.getElementById("con_log");
+let cmd = document.getElementById("cmdjs");
+let cmdData = '';
+let sendCmd = document.getElementById("inject");
+cmd.oninput = function(){
+  cmdData = this.value;
+}
+let vmjs = document.createElement("script");
+vmjs.async = "";
+document.body.appendChild(vmjs);
+sendCmd.onclick = function(){
+  vmjs.innerHTML += cmdData;
+  mainLog(">"+cmdData);
+  vmjs.innerHTML += "";
+  cmdData = "";
+}
+let d = new Date();
+function mainLog(data){
+  cntmsg++;
+  consoleBuffer.innerHTML += '<br>'+d.getHours()+":"+d.getMinutes()+":"+d.getSeconds()+" "+data;
+  console.log("\n"+d.getHours()+":"+d.getMinutes()+":"+d.getSeconds()+" "+data);
+  main_logStr += "\n"+d.getHours()+":"+d.getMinutes()+":"+d.getSeconds()+" "+data;
+  localStorage.consoleLogs += main_logStr;
+  if(cntmsg >= 20){
+    consoleBuffer.innerHTML = "Auto Clean Console!!";
+    cntmsg = 0;
+    consoleBuffer.innerHTML = "";
+  }
+}
+function SaveLog(){
+  pixliteFile("XVPN_PXngineLog.txt",main_logStr);
+}
+function pixliteFile(name, value)
+{
+       var val = value;
+      if (value === undefined) {
+      val = "";
+       }
+      var download = document.createElement("a");
+      download.href = "data:text/plain;content-disposition=attachment;filename=file," + val;
+      download.download = name;
+      download.style.display = "none";
+      download.id = "download"; document.body.appendChild(download);
+      document.getElementById("download").click();
+      document.body.removeChild(download);
+}
+mainLog("module main.js loaded "+doLoadTimeMs+"ms");
+function mainLogClear(){
+  consoleBuffer.innerHTML = "";
+  cntmsg = 0;
+  console.clear();
+}
 class Device{
   getDevice() {
       for (var i in userDeviceArray) {
@@ -52,54 +113,83 @@ class Device{
 let DeviceInfo = new Device();
 let AppInit = false;
 class AppUI{
+  CSSReset(){
+    cssLoader.rel = "stylesheet";
+    cssLoader.href = "";
+    setTimeout(function(){
+      cssLoader.href =  "././css/main.css";
+    },100);
+  }
+  AppShutdown(dbg_sw,mainContent,consoleContent){
+    switch(dbg_sw){
+      case 0:
+        mainContent.style.display = "block";
+        consoleContent.style.display = "none";
+        break;
+      case 1:
+        mainContent.style.display = "none";
+        consoleContent.style.display = "block";
+      break;
+  }
+}
+  displayParse(){
+    if(window.screen.width > window.screen.width){
+      this.AppInit();
+    }
+    if(window.screen.height > window.screen.height){
+      this.AppInit();
+    }
+  }
   AppInit(){
+    //mainLogClear();
     let ctx_display = window.screen.width;
     let cty_display = window.screen.height;
     let bMobile = false;
     AppInit = true;
-    console.log("[main.js] init class AppUI");
-    console.log("[device.js] display size offset: "+ctx_display +"x"+cty_display);
-    console.log("[main,js] loaded data:");
-    console.log("[main.js]\n=====================\n");
+    mainLog("[main.js] init class AppUI");
+    mainLog("[device.js] display size offset: "+ctx_display +"x"+cty_display);
+    mainLog("[main,js] loaded data:");
    let   fAllDivsFrames = document.getElementsByTagName('div');
    let   fAllPTags = document.getElementsByTagName('h1');
    let   fAllButtonsFrames = document.getElementsByTagName('button');
-    console.log("[main.js]\ndivs:"+fAllDivsFrames.length+"=====================\n");
-    console.log("[main.js]\np:"+fAllPTags.length+"=====================\n");
-    console.log("[main.js]\nbuttons:"+fAllButtonsFrames.length+"=====================\n");
+    mainLog("[main.js]divs:"+fAllDivsFrames.length+"=====================\n");
+    mainLog("[main.js]h:"+(fAllPTags.length + document.getElementsByTagName('h2').length)+"=====================\n");
+    mainLog("[main.js]buttons:"+fAllButtonsFrames.length+"=====================\n");
     if(DeviceInfo.getDevice() == 'Android'){
-      console.log("[device.js] device : Android");
+      mainLog("[device.js] device : Android");
       bMobile = true;
     }
     if(DeviceInfo.getDevice() == 'iPhone'){
-      console.log("[device.js] device : iPhone");
+      mainLog("[device.js] device : iPhone");
       bMobile = true;
     }
     if(bMobile){
-      console.log("[main.js] loaded css main_mobile.css");
+      mainLog("[main.js] loaded css main_mobile.css");
       cssLoader.rel = "stylesheet";
-      cssLoader.href = "././css/main_mobile.css";
+      cssLoader.href = "././css/main.css";
       document.getElementById("contentframeid").style.overflowY = "scroll";
       document.getElementById("contentframeid").style.overflowX = "hidden";
-      console.log("[main.js] Enable Scroll");
+      mainLog("[main.js] Enable Scroll");
     }
     if(DeviceInfo.getDevice() == 'Windows'){
       bMobile = false; 
-      console.log("[device.js] device : Windows");
+      mainLog("[device.js] device : Windows");
     }
     if(DeviceInfo.getDevice() == 'Linux'){
       bMobile = false;
-       console.log("[device.js] device : Linux");
+       mainLog("[device.js] device : Linux");
     }
     if(!bMobile){
-      console.log("[device.js] device :"+DeviceInfo.getDevice());
+      mainLog("[device.js] device :"+DeviceInfo.getDevice());
       if(ctx_display < 1920){
         document.getElementById("contentframeid").style.overflowY = "scroll";
         document.getElementById("contentframeid").style.overflowX = "hidden";
-        console.log("[device.js] you display size offset: ctx_display:"+ctx_display+" < 1920");
-        console.log("[main.js] Enable Scroll");
+        mainLog("[device.js] you display size offset: ctx_display:"+ctx_display+" < 1920");
+        mainLog("[main.js] Enable Scroll");
       }
     }
+    this.toLoadTimeMs = parseFloat(new Date().getMilliseconds());
+    mainLog("[main.js] App init time "+(doLoadTimeMs)+"ms");
   }
    LangUI(id_lang){
     if(!AppInit){
@@ -108,47 +198,50 @@ class AppUI{
       window.location.href = "";
     }
     if(!id_lang){langButton.innerHTML = "RU";
+      mainLog("[main.js] lang selected: RU");
       tx_0.innerHTML = stringData[0];
-    tx_1.innerHTML = stringData[6];
+    //tx_1.innerHTML = stringData[6];
     tx_2.innerHTML = stringData[7];
     tx_3.innerHTML = stringData[10];
     tx_4.innerHTML = stringData[11];
     tx_5.innerHTML = stringData[1];
-    tx_6.innerHTML = stringData[6];
+  //  tx_6.innerHTML = stringData[6];
     tx_7.innerHTML = stringData[8];
     tx_8.innerHTML = stringData[10];
-    tx_9.innerHTML = stringData[11];
+    tx_9.innerHTML = stringData[13];
     tx_A0.innerHTML = stringData[2];
-    tx_A1.innerHTML = stringData[6];
+    //tx_A1.innerHTML = stringData[6];
     tx_A2.innerHTML = stringData[9];
     tx_A3.innerHTML = stringData[10];
     tx_A4.innerHTML = stringData[12];
     }
     if(id_lang){langButton.innerHTML = "EN";
+      mainLog("[main.js] lang selected: EN");
       tx_0.innerHTML = stringData[14];
-    tx_1.innerHTML = stringData[21];
+    //tx_1.innerHTML = stringData[21];
     tx_2.innerHTML = stringData[18];
     tx_3.innerHTML = stringData[22];
     tx_4.innerHTML = stringData[23];
     tx_5.innerHTML = stringData[15];
-    tx_6.innerHTML = stringData[21];
+    //tx_6.innerHTML = stringData[21];
     tx_7.innerHTML = stringData[19];
     tx_8.innerHTML = stringData[22];
-    tx_9.innerHTML = stringData[24];
+    tx_9.innerHTML = stringData[25];
     tx_A0.innerHTML = stringData[16];
-    tx_A1.innerHTML = stringData[21];
+    //tx_A1.innerHTML = stringData[21];
     tx_A2.innerHTML = stringData[20];
     tx_A3.innerHTML = stringData[22];
-    tx_A4.innerHTML = stringData[25];
+    tx_A4.innerHTML = stringData[24];
     }
    }
    bodyDataLast = document.body.outerHTML;
    buttonBuyUI(id){
+    mainLog("[main.js] buy_window id:"+id);
     document.getElementById("payment_data").style.display = "block";
     document.getElementById("payment_data").style.width  = 609+"px";
     document.getElementById("payment_data").style.height  = 196+"px";
     document.getElementById("payment_data").style.transform = "translate("+(( window.screen.width / 2 ) / 2)+"px,"+(( window.screen.height / 2) / 2)+"px)";
-    console.log("[main.js] payment_data: window id to "+"translate("+(( window.screen.width / 2 ) / 2)+"px,"+(( window.screen.height / 2) / 2)+"px)");
+    mainLog("[main.js] payment_data: window id to "+"translate("+(( window.screen.width / 2 ) / 2)+"px,"+(( window.screen.height / 2) / 2)+"px)");
     //window.open("https://t.me/hcppstudio");
    }
 }
@@ -176,6 +269,30 @@ let langButton = document.getElementById("lang_btnid");
 
 let AppHandle = new AppUI();
 //
+let consoleHandle = document.getElementById("console");
+let mainHandle = document.getElementById("contentframeid");
+let conState = document.getElementById("conState");
+let cssReload = document.getElementById("css_reload");
+let jsReload = document.getElementById("js_reload");
+conState.onclick = function(){
+  AppHandle.AppShutdown(0,mainHandle,consoleHandle);
+}
+
+cssReload.onclick = function(){
+  AppHandle.CSSReset();
+}
+jsReload.onclick = function(){
+  document.getElementById("module").src = "";
+  setTimeout(function(){
+    document.getElementById("module").src = "main.js";
+    document.getElementById("module").onload = AppHandle.AppInit();
+  },100);
+  mainLog("[swap_js] reload javascript");
+}
+let conState1 = document.getElementById("conOn");
+conState1.onclick = function(){
+  AppHandle.AppShutdown(1,mainHandle,consoleHandle);
+}
 let langId = 0;
 langButton.onclick = function(){
   langId++;
@@ -184,4 +301,6 @@ langButton.onclick = function(){
   }
   AppHandle.LangUI(langId);
 }
-
+setInterval(function(){
+  AppHandle.displayParse();
+},10);
